eafe8d4437f7cab4a3f7147509288f4cb0edbd29
davisk4rpi@gmail.com <davisk4rpi@gmail.com>
2017-01-06 23:50:53 -0800

    Create Piece classes initializing with starting positions

---
+++ lib/chess_pieces.rb
@@ -0,0 +1,51 @@
+module ChessPieces
+	
+	class Pawn
+
+		def initialize(position)
+			@position = position
+		end
+
+	end
+
+	class Rook
+
+		def initialize(position)
+			@position = position
+		end
+
+	end
+
+	class Knight
+
+		def initialize(position)
+			@position = position
+		end
+
+	end
+
+	class Bishop
+
+		def initialize(position)
+			@position = position
+		end
+
+	end
+
+	class Queen
+
+		def initialize(position)
+			@position = position
+		end
+
+	end
+
+	class King
+
+		def initialize(position)
+			@position = position
+		end
+
+	end
+
+end
